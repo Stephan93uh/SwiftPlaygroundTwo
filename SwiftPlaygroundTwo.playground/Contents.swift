0739074957ba86c5babbe75940bcb868099e2716
Stephan93uh <stephan9@hawaii.edu>
2022-09-15 19:49:55 -1000

    Completed Parts 1-4

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var goodGrade = "I've been doing all my work"
+
+//Part #2
+if goodGrade == "I've been doing all my work" {
+    print("I should be getting an A at this point")
+}
+
+//Part #3
+if goodGrade == "I'm not doing very well" {
+    print ("I should be geeting an A at this point")
+}
+else {
+    print ("I need to work harder on this class")
+}
+
+//Part 4
+var gradeA = true
+if gradeA == true {
+    print ("I really like this class")
+}
+else {
+    print ("I'm not wo sure about coding in Swift")
+}
